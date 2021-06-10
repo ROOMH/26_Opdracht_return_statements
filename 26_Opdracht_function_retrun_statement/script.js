18b6af8c31776dda9b917a77733021d18dacd432
Romy <80348985+ROOMH@users.noreply.github.com>
2021-06-10 12:01:59 +0200

    Add files via upload

---
+++ 26_Opdracht_function_retrun_statement/script.js
@@ -0,0 +1,34 @@
+// Checking if a number is big 
+
+/* const biggerThen = function (number) {
+    console.log("20");
+    return true;
+    if ("<100") {
+        return false;
+    }
+} 
+biggerThen(">100");*/
+
+// This is a produce something 
+
+
+// Bouncer at a club 
+// Max, age, volwassenen
+// Kom binnen, het is te druk, voor volwassenen
+
+const add = function (number1, number2, number3) {
+    const max= number1;
+    if (max >200) {
+        return "Het is te druk, komt later terug";
+    }
+    const age= number2;
+    if (age =>16) {
+        return "Kom binnen";
+    }
+    const adult= number3;
+    if (adult <21);
+    return "Deze club is voor volwassenen";
+}; 
+
+const result= add(200, 14, 22 );
+console.log(result);
